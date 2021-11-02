6cd0e3efeddc331136c9635292ea373385e66cd1
jeongseonwoong <tjsdnd1219@naver.com>
2021-11-02 17:18:01 +0900

    Test 브랜치에 커밋을 추가함

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,8 +2,13 @@
 
 int main()
 {
+  int i=0;
+
     printf("First OSS Code\n");
     printf("second OSS code\n");
-    
+
+    for(int i=0;i<10;i++){
+      printf("Test 브랜치\n");
+    }
     return 0;
 }
